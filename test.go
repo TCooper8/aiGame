82a644cf7b6a92598c5e110eb7df0b6c6a440dc3
Tyler Cooper <tyler.cooper@tcnbroadcasting.com>
2016-01-12 16:39:35 -0700

    Basic publish and response queue listener works

--- test.go
+++ test.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-  "errors"
   "github.com/tcooper8/aiGame/logging"
-  "github.com/tcooper8/aiGame/eventBus"
+  "github.com/tcooper8/aiGame/events"
 )
 
 const (
@@ -11,30 +10,61 @@ const (
   NOT_IMPLEMENTED = "Method not implemented"
 )
 
-func consumeRegister(stream <-chan *EventRequest) {
-  for event := stream {
-
-  }
-}
-
-func consumeRegister(msg *events.AuthRegister) *AuthRegisterReply {
-  return &AuthRegisterReply{
-    success : false,
-    err     : NOT_IMPLEMENTED,
+func consumeRegister(msg *events.AuthRegister) *events.AuthRegisterReply {
+  return &events.AuthRegisterReply{
+    Success : false,
+    Err     : NOT_IMPLEMENTED,
   }
 }
 
 func main() {
+  log := logging.New("service", logging.INFO)
+
   bus, err := events.NewAmqpPool("amqpPool", logging.INFO)
   if err != nil {
     panic(err)
   }
 
-  respondsOn := events.AuthGroup
-  respondsOn := make(map[string], chan interface{})
+  consumer := make(chan *events.EventRequest)
 
-  bus.SetMapping(&AmqpEventMapping{
+  bus.SetConsumer("auth.register", consumer)
+  bus.SetConsumer("auth.register.response", consumer)
+
+  log.Info("Running")
+
+  err = bus.SetMapping(&events.AmqpEventMapping{
     EndPoint    : "amqp://localhost:5672/",
-    RespondsOn  : make(map[string],
+    EventType   : "auth.register",
+    Exchange    : "auth",
+    ReqQueue    : "auth.register",
   })
+  if err != nil {
+    log.Error("SetMapping error: %s", err)
+  }
+
+  err = bus.SetMapping(&events.AmqpEventMapping{
+    EndPoint    : "amqp://localhost:5672/",
+    EventType   : "auth.register.response",
+    Exchange    : "auth",
+    ReqQueue    : "auth.register.response",
+  })
+  if err != nil {
+    log.Error("SetMapping error: %s", err)
+  }
+
+  for req := range(consumer) {
+    switch msg := req.Request.(type) {
+    default:
+      log.Warn("Received unhandled msg: %T %s", msg, msg)
+
+    case *events.AuthRegisterReply:
+      log.Info("Got reply %s", msg)
+
+    case events.AuthRegister:
+      req.Reply <- consumeRegister(&msg)
+
+    case *events.AuthRegister:
+      req.Reply <- consumeRegister(msg)
+    }
+  }
 }
