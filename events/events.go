311e59cb09d200612775f0d2b9eb1b2fab0b5e56
Tyler Cooper <tyler.cooper@tcnbroadcasting.com>
2016-01-12 16:39:11 -0700

    Added AuthRegisterReply to typeMapping

--- events/events.go
+++ events/events.go
@@ -5,33 +5,42 @@ import (
 )
 
 type AuthRegisterReply struct {
-  success   bool
-  err       string
+  Success   bool
+  Err       string
 }
 
 type AuthRegister struct {
-  Username  string
-  Password  string
-  Email     string
+  Username  string  `json:"username"`
+  Password  string  `json:"password"`
+  Email     string  `json:"email"`
 }
 
 type EventRequest struct {
   Request interface{}
-  Reply   <-chan interface{}
+  Reply   chan<- interface{}
 }
 
-var typeMapping map[string] *reflect.Type
-var replyMapping map[string] *reflect.Type
+var typeMapping map[string] reflect.Type
+var replyMapping map[string] reflect.Type
 
 func init() {
-  typeMapping[AuthRegister.Name()] = AuthRegister
-  replyMapping[AuthRegister.Name()] = AuthRegisterReply
+  typeMapping = make(map[string] reflect.Type)
+  replyMapping = make(map[string] reflect.Type)
+
+  v0 := AuthRegister{}
+  typeMapping["auth.register"] = reflect.TypeOf(v0)
+
+  v1 := AuthRegisterReply{}
+  replyMapping["auth.register"] = reflect.TypeOf(v1)
+  typeMapping["auth.register.response"] = reflect.TypeOf(v1)
 }
 
-func GetType(eventType string) (*reflect.Type, bool) {
-  return typeMapping[eventType]
+func GetType(eventType string) (reflect.Type, bool) {
+  val, ok := typeMapping[eventType]
+  return val, ok
 }
 
-func GetResponseType(eventType string) (*reflect.Type, bool) {
-  return replyMapping[eventType]
+func GetResponseType(eventType string) (reflect.Type, bool) {
+  val, ok := replyMapping[eventType]
+  return val, ok
 }
